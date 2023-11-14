f0e532248a2fb4e37998ed294a2cfffcec9c7c77
Moniappdeveloper <royalnorwork@gmail.com>
2023-11-14 11:37:14 +0100

    Added locals

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
+locals {
+  workspaces_suffix = terraform.workspace =="default"? "" : "${terraform.workspace}"
+  rg_name_backend = "${var.rg_name_backend}- ${locals.workspaces_suffit}"
+}
+
 
 
 resource "azurerm_resource_group" "rg-demo-backend" {
-  name     = var.rg_name_backend
+  name     = local.rg_name_backend
   location = var.rg_backend_location
 }
 
+output "rg_name_backend" {
+    value = azurerm_resource_group.rg_name_backend
+  
+}
